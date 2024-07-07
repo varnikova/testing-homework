aa5cdc748fe7a4419cbbe307bbe50e17c6f79eb0
Daria Varnikova <varnikowa@gmail.com>
2024-07-07 16:35:02 +0300

    undo js files

--- src/client/components/CartBadge.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { cn } from '@bem-react/classname';
-import { useSelector } from 'react-redux';
-var bem = cn('CartBadge');
-export var CartBadge = function (_a) {
-    var id = _a.id;
-    var cart = useSelector(function (s) { return s.cart; });
-    return cart[id] ? React.createElement("span", { className: bem(null, ['text-success', 'mx-3']) }, "Item in cart") : null;
-};
--- src/client/components/Image.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { cn } from '@bem-react/classname';
-var STUB = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkMAYAADkANVKH3ScAAAAASUVORK5CYII=';
-var bem = cn('Image');
-export var Image = function (_a) {
-    var className = _a.className, _b = _a.src, src = _b === void 0 ? STUB : _b;
-    return React.createElement("img", { className: bem(null, [className]), src: src });
-};
